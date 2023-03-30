a2a9e92c83b4d5d8057f70dcb13954e550127f7f
prashanthakur <thakurprince9910@gmail.com>
2023-03-30 13:11:39 +0530

    fst cmt

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose')
+const PORT= process.env.PORT || 5000;
+const {MONGOURI} = require('./config/keys')
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*'
+}));
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+mongoose.connection.on('connected',()=>{
+    console.log("connected to mongoDb Yeah!")
+})
+mongoose.connection.on('error',()=>{
+    console.log("Error in connecting")
+})
+
+require('./models/user')
+require('./models/post')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
+
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
